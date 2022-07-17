2bacfd94ce81e90ab34a578456897583ee900e16
alkihuri <alkihuri@gmail.com>
2022-07-17 03:25:35 +0300

    almost MVP

--- Bot/BotManager.py
+++ Bot/BotManager.py
@@ -2,25 +2,80 @@ from email import message
 from pydoc import cli
 import telebot
 import BotSettings
+import pyautogui
+import os
+from datetime import datetime
 
 
-def Test(msgs):
-    for m in msgs:
-        chatid = m.chat.id
-        if m.content_type == "text":
-            client.send_message(chat_id=chatid,text=m.text)
+spends = []
+
+def UpdateDB(chat_id,cat,amount): 
+    path = os.getcwd()
+    dbPath = path+"/"+str(chat_id)+"_db.csv" 
+
+    try:
+        data = open(dbPath,"r+") 
+    except:
+        data = open(dbPath,"w+") 
         
-client = telebot.TeleBot(BotSettings.token)
+    db = data.readlines()
+    
+    total = 0
+    try:
+        for x in db:
+            if x.find(cat) > -1:
+                try:
+                    total += int( x.split(";")[2])
+                except:
+                    total += 0
+    
+        total += int(amount)
+        total =str(total)
 
-client.set_update_listener(Test)
+    except:
+        total = amount
+    
+    data.close()
+    newSpend = BotSettings.Spend()
+    newSpend.user = chat_id
+    newSpend.category = cat
+    newSpend.amount = amount  
+    spends.clear()
+    info = "\n" + str( chat_id) + ";" +  str(newSpend.category) + ";" + str(newSpend.amount) + ";" + str(datetime.now())
+    data = open(dbPath,"a")
+    data.write(info)
+    data.close()
+    client.send_message(chat_id,"бот записал ваш расход...")  
+    client.send_message(chat_id,"вы уже потратили в этой категории..." + total)
+
+def MakeSceenShot():
+    myScreenshot = pyautogui.screenshot() 
+    return myScreenshot
+
+       
+client = telebot.TeleBot(BotSettings.token)
+ 
 
 @client.message_handler(content_types=['text'])
 def TestFunc(message):
-    msg = message.text 
-    if msg.lower() == "ping": 
-        client.send_message(message.chat.id,"pong")
-        client.send_location(message.chat.id,latitude=44,longitude=22)
+    msg = message.text  
+    
+    try:
+        cat = msg.split(" ")[0] 
+        amo = msg.split(" ")[1]
+        UpdateDB(message.chat.id,cat,amo)  
+    except: 
+        client.send_message(message.chat.id,"Введите данные в коректном формате [категория деньги] ")  
 
 
+@client.message_handler(commands=['clear'])
+def Clear(message): 
+    path = os.getcwd()
+    dbPath = path+"/"+str(message.chat.id)+"_db.csv"   
+    data = open(dbPath,"w+") 
+    data.close()
+    client.send_message(message.chat.id,"Данные удалены...")
 
-client.polling(non_stop=True,interval=0)
+
+ 
+client.polling(non_stop=True,interval=1)
--- Bot/BotSettings.py
+++ Bot/BotSettings.py
@@ -1,1 +1,6 @@
 token = "5499026409:AAGV3com_c4nmndYwErEyuJJXpCn-YfK0qQ"
+ 
+class Spend:
+    user = ""
+    category = ""
+    amount = ""
