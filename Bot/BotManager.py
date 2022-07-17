33bc91bfcc2a3d1df559d1d08cd777570f0bdd10
alkihuri <alkihuri@gmail.com>
2022-07-17 19:51:14 +0300

    sqlite feature

--- Bot/BotManager.py
+++ Bot/BotManager.py
@@ -1,81 +1,75 @@
-from email import message
+# -*- coding: utf-8 -*-
+
 from pydoc import cli
 import telebot
 import BotSettings
 import pyautogui
 import os
 from datetime import datetime
+import sqlite3
+
 
 
 spends = []
-
-def UpdateDB(chat_id,cat,amount): 
-    path = os.getcwd()
-    dbPath = path+"/"+str(chat_id)+"_db.csv" 
-
-    try:
-        data = open(dbPath,"r+") 
-    except:
-        data = open(dbPath,"w+") 
-        
-    db = data.readlines()
-    
-    total = 0
-    try:
-        for x in db:
-            if x.find(cat) > -1:
-                try:
-                    total += int( x.split(";")[2])
-                except:
-                    total += 0
-    
-        total += int(amount)
-        total =str(total)
-
-    except:
-        total = amount
-    
-    data.close()
-    newSpend = BotSettings.Spend()
-    newSpend.user = chat_id
-    newSpend.category = cat
-    newSpend.amount = amount  
-    spends.clear()
-    info = "\n" + str( chat_id) + ";" +  str(newSpend.category) + ";" + str(newSpend.amount) + ";" + str(datetime.now())
-    data = open(dbPath,"a")
-    data.write(info)
-    data.close()
-    client.send_message(chat_id,"бот записал ваш расход...")  
-    client.send_message(chat_id,"вы уже потратили в этой категории..." + total)
-
-def MakeSceenShot():
-    myScreenshot = pyautogui.screenshot() 
-    return myScreenshot
-
-       
 client = telebot.TeleBot(BotSettings.token)
+
+def InsertValues(message,cat,amount): 
+    chat_id = str(message.chat.id)
+    db = BotSettings.InnitDataBase(message)
+    cur = db.cursor()
+    cur.execute('''INSERT INTO SPENDS VALUES (?, ?, ?,?)''',(chat_id,cat,amount,datetime.now())) 
+    db.commit() 
+    db.close()
+    client.send_message(message.chat.id,"Ваш расход добавлен в таблицу...")  
+
+        
+
+@client.message_handler(commands=['start'])
+def StartApp(message): 
+    db = BotSettings.InnitDataBase(message)
  
 
-@client.message_handler(content_types=['text'])
-def TestFunc(message):
-    msg = message.text  
+@client.message_handler(commands=['getall'])
+def GetAll(message):  
+    db = BotSettings.InnitDataBase(message)
+    cur = db.cursor()
+    data = cur.execute('''SELECT * FROM SPENDS''')    
+    client.send_message(message.chat.id,"Список ваших трат:")  
+    for line in list(data): 
+        print(line)
+        data =  str(line[1])+ " - " + str(line[2]) + " время : " + str(line[3])
+        client.send_message(message.chat.id,data) 
+    if(len(list(data)) == 0): 
+     client.send_message(message.chat.id,"пусто.")  
+
     
+    db.close()
+
+@client.message_handler(commands=['clear'])
+def GetAll(message):  
+    db = BotSettings.InnitDataBase(message)
+    cur = db.cursor()
+    cur.execute('''DELETE FROM SPENDS''')  
+    db.commit()
+    client.send_message(message.chat.id,"Данные удалены...")   
+    db.close()
+
+
+@client.message_handler(func=lambda message: True, content_types=['text'])
+def EachMsgFunc(message):
+    msg = message.text   
+    
+    if msg.find('/') > -1:
+        return
+ 
+    print(msg) 
     try:
         cat = msg.split(" ")[0] 
-        amo = msg.split(" ")[1]
-        UpdateDB(message.chat.id,cat,amo)  
-    except: 
+        amo = msg.split(" ")[1] 
+        InsertValues(message,cat,amo)  
+    except:
         client.send_message(message.chat.id,"Введите данные в коректном формате [категория деньги] ")  
 
 
-@client.message_handler(commands=['clear'])
-def Clear(message): 
-    path = os.getcwd()
-    dbPath = path+"/"+str(message.chat.id)+"_db.csv"   
-    data = open(dbPath,"w+") 
-    data.close()
-    client.send_message(message.chat.id,"Данные удалены...")
-
-
  
-client.polling(non_stop=True,interval=1)
+client.polling(non_stop=True,interval=1) 
--- Bot/BotSettings.py
+++ Bot/BotSettings.py
@@ -1,6 +1,27 @@
+
+import re
+import sqlite3 
+import telebot
+
 token = "5499026409:AAGV3com_c4nmndYwErEyuJJXpCn-YfK0qQ"
  
 class Spend:
     user = ""
     category = ""
     amount = ""
+
+
+def InnitDataBase(message): 
+    dbName = str( message.chat.id)
+    con = sqlite3.connect(dbName+'.db')
+    сur = con.cursor()
+    сur.execute('''CREATE TABLE IF NOT EXISTS SPENDS
+                (user text, category text,amount real,time text)''') 
+    con.commit()
+    return con
+
+def InnitCursor(message):
+    db = InnitDataBase(message)
+    return db.cursor()
+ 
+
